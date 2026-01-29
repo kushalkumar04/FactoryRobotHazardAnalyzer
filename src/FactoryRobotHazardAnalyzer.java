42c858fcd9623f59e94e68b6ad038f7ccb82290f
Kushal Kumar <gaddamkushal321@gmail.com>
2026-01-29 17:11:18 +0530

    [kushal] UC4: Add input validation using if-else logic

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -19,10 +19,31 @@ public class FactoryRobotHazardAnalyzer {
 
         double machineRiskFactor = 1.0;
 
-        double hazardRisk =
-                ((1.0 - armPrecision) * 15.0)
-                        + (workerDensity * machineRiskFactor);
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            System.out.println("Error: Arm precision must be 0.0-1.0");
+        } else if (workerDensity < 1 || workerDensity > 20) {
+            System.out.println("Error: Worker density must be 1-20");
+        } else if (
+                !machineryState.equals("Worn") &&
+                        !machineryState.equals("Faulty") &&
+                        !machineryState.equals("Critical")) {
 
-        System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+            System.out.println("Error: Unsupported machinery state");
+        } else {
+
+            if (machineryState.equals("Worn")) {
+                machineRiskFactor = 1.3;
+            } else if (machineryState.equals("Faulty")) {
+                machineRiskFactor = 2.0;
+            } else if (machineryState.equals("Critical")) {
+                machineRiskFactor = 3.0;
+            }
+
+            double hazardRisk =
+                    ((1.0 - armPrecision) * 15.0)
+                            + (workerDensity * machineRiskFactor);
+
+            System.out.println("Robot Hazard Risk Score: " + hazardRisk);
+        }
     }
 }
