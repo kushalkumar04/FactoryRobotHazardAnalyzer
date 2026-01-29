ab64d545bb35a2d46c0417c8ed49bff423d20d6e
Kushal Kumar <gaddamkushal321@gmail.com>
2026-01-29 17:50:26 +0530

    [kushal] UC7: separate hazard logic into RobotHazardAuditor

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -18,7 +18,9 @@ public class FactoryRobotHazardAnalyzer {
             System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
             String machineryState = scanner.nextLine();
 
-            double risk = calculateHazardRisk(
+            RobotHazardAuditor auditor = new RobotHazardAuditor();
+
+            double risk = auditor.calculateHazardRisk(
                     armPrecision, workerDensity, machineryState);
 
             System.out.println("Robot Hazard Risk Score: " + risk);
@@ -26,38 +28,5 @@ public class FactoryRobotHazardAnalyzer {
         } catch (RobotSafetyException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static double calculateHazardRisk(
-            double armPrecision,
-            int workerDensity,
-            String machineryState)
-            throws RobotSafetyException {
-
-        double machineRiskFactor;
-
-        if (armPrecision < 0.0 || armPrecision > 1.0) {
-            throw new RobotSafetyException(
-                    "Error: Arm precision must be 0.0-1.0");
-        }
-
-        if (workerDensity < 1 || workerDensity > 20) {
-            throw new RobotSafetyException(
-                    "Error: Worker density must be 1-20");
-        }
-
-        if (machineryState.equals("Worn")) {
-            machineRiskFactor = 1.3;
-        } else if (machineryState.equals("Faulty")) {
-            machineRiskFactor = 2.0;
-        } else if (machineryState.equals("Critical")) {
-            machineRiskFactor = 3.0;
-        } else {
-            throw new RobotSafetyException(
-                    "Error: Unsupported machinery state");
-        }
-
-        return ((1.0 - armPrecision) * 15.0)
-                + (workerDensity * machineRiskFactor);
     }
 }
